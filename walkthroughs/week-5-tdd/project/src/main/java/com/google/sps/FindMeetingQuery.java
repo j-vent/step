762effeddd4165cc1352222b7cc964725c915fd1
j-vent <justine.ventura14@gmail.com>
2020-07-14 21:48:42 +0000

    refactor code to get free timeslots

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -11,50 +11,80 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 package com.google.sps;
-
+ 
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-
-
+ 
+ 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    List<Integer> fullDay = new ArrayList<Integer>();
-    
-    for(int i=0; i < 2531;i=i+30){
-        fullDay.add(i);
-    }
-    System.out.println(fullDay);
+     List<TimeRange> fullDay = new ArrayList<TimeRange>();
+    // List<TimeRange> fullDay = new Set<TimeRange>();
+    fullDay.add(TimeRange.WHOLE_DAY);
+   
+
+    List<TimeRange> availableTimes = new ArrayList<TimeRange>();
     
     // throw new UnsupportedOperationException("TODO: Implement this method.");
     
     Collection<String> attendees = request.getAttendees();
+ 
     // this is some horrific n^3 runtime
+
+    // cannot modify arraylist in an enhanced for loop, must create separate arraylists
+    List<TimeRange> freeTimes = new ArrayList<TimeRange>();
+    List<TimeRange> blockedTimes = new ArrayList<TimeRange>();
+    TimeRange freetime;
     for(Event event: events){
         Set<String> eventAttendees = event.getAttendees();
         for(String attendee:eventAttendees){
             if(attendees.contains(attendee)){
-                TimeRange blocked = event.getWhen();
-                System.out.println(blocked);
-                // mark out booked timeslots with -1
-                int start = blocked.start()/30;
-                int end = blocked.end()/30;
-                for(int j = start; j < end; j++){
-                    fullDay.set(j,-1);
+              TimeRange blocked = event.getWhen();
+              int start = blocked.start();
+              int end = blocked.end();
+            
+              for(TimeRange timeslot: fullDay){
+                if(blocked.overlaps(timeslot)){
+                     // top overlap: add a timeslot before this event
+                    if(start >= timeslot.start() && start < timeslot.end()){
+                      freetime = TimeRange.fromStartEnd(timeslot.start(), start,false);
+                      if(!fullDay.contains(freetime)){
+                        freeTimes.add(freetime);
+                      }
+                     
+                    }
+                    if(end >= timeslot.start() && end < timeslot.end()){
+                      // bottom overlap: add a timeslot after this event
+                      freetime = TimeRange.fromStartEnd(end,timeslot.end(),false);
+                      if(!fullDay.contains(freetime)){
+                        freeTimes.add(freetime);
+                      }
+                    }
+                    blockedTimes.add(timeslot);
                 }
+              }
+              fullDay.removeAll(blockedTimes);
+              fullDay.addAll(freeTimes);
+
                 
             }
         }
     }
-
-    System.out.println(fullDay);
-    return new ArrayList<TimeRange>();
+    //System.out.println("fullday");
+    //System.out.println(fullDay);
     // get all the remaining, free time slots
-    // filter time slots by required duration of meeting
+    
+    // List<TimeRange> availableTimes = new ArrayList<TimeRange>();
+    return fullDay;
   }
 }
+ 
+ 
+ 
+
